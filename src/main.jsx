5ef5fadf0863b0b7c72293ceb26ef0868748c088
shmas khan <sktanoli420@gmail.com>
2024-01-30 16:39:27 +0500

    lazy load

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Contact from "./pages/Contact.jsx"
+// import Contact from "./pages/Contact.jsx"
 import Home from "./pages/Home.jsx"
 // import About from "./pages/About.jsx"
 import { lazy } from "react"
 const About = lazy(() => import("./pages/About.jsx"))
+const Contact = lazy(() => import("./pages/Contact.jsx"))
 
 const router = createBrowserRouter([
 	{
